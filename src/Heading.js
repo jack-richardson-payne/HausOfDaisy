7c09e2f667023648b4a137ffc5a745df9234f155
Jack Richardson-Payne <jack.richardson-payne@outlook.com>
2020-07-13 14:50:08 +0100

    pillows -> cushions

--- src/Heading.js
+++ src/Heading.js
@@ -8,7 +8,7 @@ class Heading extends React.Component{
         <a class="MLeft" href="index.html">Home</a>
         <a class="BLeft" href="about.html">About</a>
         <img class="logo" src='./images/logo.PNG' alt="Haus Of Daisy"/>
-        <a href="pillows.html">Pillows</a>
+        <a href="cushions.html">Cushions</a>
         <a class="BLeft" href="prints.html">Prints</a>
         <div class="instaLink MRight">
           <p>Check out these links!</p>
